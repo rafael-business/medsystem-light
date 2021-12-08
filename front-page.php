73bcad5d15f348ff33a48a50a35e6b895550d03f
Rafael dos Santos <rafael@olivasdigital.com.br>
2021-12-08 10:30:22 -0300

    Tema atualizado

--- front-page.php
+++ front-page.php
@@ -8,6 +8,56 @@
  */
 
 get_header();
+
+function calculate_signature( $string, $private_key ) {
+    $hash = hash_hmac( 'sha1', $string, $private_key, true );
+    $sig = rawurlencode( base64_encode( $hash ) );
+    return $sig;
+}
+ 
+$base_url = 'https://telemedic.top/gravityformsapi/';
+$api_key = '977c815ee8';
+$private_key = '2b98e9a066b4813';
+$method  = 'GET';
+$route = 'forms/6/entries';
+$expires = strtotime( '+60 mins' );
+$string_to_sign = sprintf( '%s:%s:%s:%s', $api_key, $method, $route, $expires );
+$sig = self::calculate_signature( $string_to_sign, $private_key );
+ 
+$url = $base_url . $route . '?api_key=' . $api_key . '&signature=' . $sig . '&expires=' . $expires;
+ 
+$response = wp_remote_request( $url, array('method' => 'GET' ) );
+ 
+if ( wp_remote_retrieve_response_code( $response ) != 200 || ( empty( wp_remote_retrieve_body( $response ) ) ) ){
+    //http request failed
+    echo 'There was an error attempting to access the API.';
+    die();
+}
+ 
+$body_json = wp_remote_retrieve_body( $response );
+//results are in the "body" and are json encoded, decode them and put into an array
+$body = json_decode( $body_json, true );
+ 
+$data            = $body['response'];
+$status_code     = $body['status'];
+$total           = 0;
+$total_retrieved = 0;
+ 
+if ( $status_code <= 202 ){
+    //entries retrieved successfully
+    $entries = $data['entries'];
+    $status  = $status_code;
+    $total              = $data['total_count'];
+    $total_retrieved    = count( $entries );
+}
+else {
+    //entry retrieval failed, get error information
+    $error_code         = $data['code'];
+    $error_message      = $data['message'];
+    $error_data         = isset( $data['data'] ) ? $data['data'] : '';
+    $status             = $status_code . ' - ' . $error_code . ' ' . $error_message . ' ' . $error_data;
+}
+
 ?>
 
 	<main id="primary" class="site-main container pl-4 pr-4">
